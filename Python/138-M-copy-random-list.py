b27a40fda06575e11647279e2e8d65b4952d8722
Liam Han <zexihan@163.com>
2017-07-09 19:08:21 +0800

    update solution

--- Python/138-M-copy-random-list.py
+++ Python/138-M-copy-random-list.py
@@ -5,29 +5,32 @@ class RandomListNode(object):
         self.next = None
         self.random = None
 
-# Time:
-# Space:
+# Time: O(n)
+# Space: O(n)
+# three pass
 class Solution_1(object):
     def copyRandomList(self, head):
         """
         :type head: RandomListNode
         :rtype: RandomListNode
         """
-        if head == None: return None
-
+        if head == None: 
+            return None
+        # first iteration to duplicate original nodes to the list
+        # 1->2->3 to 1->1->2->2->3->3
         tmp = head
         while tmp:
             newNode = RandomListNode(tmp.label)
             newNode.next = tmp.next
             tmp.next = newNode
             tmp = tmp.next.next
-
+        # second iteration to copy random pointers
         tmp = head
         while tmp:
             if tmp.random:
                 tmp.next.random = tmp.random.next
             tmp = tmp.next.next
-
+        # third iteration to separate old and new lists
         newhead = head.next
         pold = head
         pnew = newhead
@@ -41,36 +44,38 @@ class Solution_1(object):
         return newhead
 
 
-# Time:
-# Space:
+# Time: O(n)
+# Space: O(n)
+# one pass
 class Solution_2(object):
     def copyRandomList(self, head):
-        if head == None: return None
+        if head == None: 
+            return None
 
         dict = {}
         cur = head
         while cur:
             # create nodes of cur, next, ran
-            if cur in dict.keys():
+            if id(cur) not in dict.keys():
                 curCopy = RandomListNode(cur.label)
-                dict[cur] = curCopy
+                dict[id(cur)] = curCopy
 
-            if cur.next and cur.next not in dict.keys():
+            if cur.next and id(cur.next) not in dict.keys():
                 nextCopy = RandomListNode(cur.next.label)
-                dict[cur.next] = nextCopy
+                dict[id(cur.next)] = nextCopy
 
-            if cur.random and cur.random not in dict.keys():
+            if cur.random and id(cur.random) not in dict.keys():
                 randomCopy = RandomListNode(cur.random.label)
-                dict[cur.random] = randomCopy
+                dict[id(cur.random)] = randomCopy
 
             # connect curCopy with nextCopy, ranCopy
             if cur.next:
-                dict[cur].next = dict[cur.next]
+                dict[id(cur)].next = dict[id(cur.next)]
 
             if cur.random:
-                dict[cur].random = dict[cur.random]
+                dict[id(cur)].random = dict[id(cur.random)]
 
             # next round
             cur = cur.next
-
-        return dict[head]
+        
+        return dict[id(head)]

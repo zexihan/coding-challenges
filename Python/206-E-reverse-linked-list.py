135543bd1a147bcea19716e4cbccbabccd82cab8
Zexi Han <zexihan@me.com>
2018-11-25 20:05:23 -0500

    update

--- Python/206-E-reverse-linked-list.py
+++ Python/206-E-reverse-linked-list.py
@@ -4,7 +4,8 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+# Iterative
+class Solution_1:
     def reverseList(self, head):
         """
         :type head: ListNode
@@ -20,3 +21,16 @@ class Solution(object):
             curr = next
             
         return prev
+
+# Recursive
+class Solution_2:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next: return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p

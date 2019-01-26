85b339a2132a7da53c167521a889cbe8c0e3fafb
Zexi Han <zexihan@me.com>
2019-01-26 09:48:16 -0500

    update

---
+++ Python/230-M-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        node = root
+        while node:
+            stack.append(node)
+            node = node.left
+        x = 1
+        while stack and x <= k:
+            node = stack.pop()
+            x += 1
+            right = node.right
+            while right:
+                stack.append(right)
+                right = right.left
+        return node.val
---
+++ Python/328-M-odd-even-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return
+        odd = head
+        even = head.next
+        evenHead = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head

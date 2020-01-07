649e0fc64114e7800edf6c879ab8cf9340fb545f
Zexi Han <zexihan@me.com>
2020-01-07 00:24:24 -0800

    add challenges

--- leetcode_151_cpp/2.1.1_remove_duplicates_from_sorted_array.cpp
+++ leetcode_151_cpp/2.1.1_remove_duplicates_from_sorted_array.cpp
@@ -1,4 +1,7 @@
-class Solution {
+// Time: O(n)
+// Space: O(1)
+class Solution 
+{
 public:
     int removeDuplicates(vector<int>& nums) 
     {
@@ -12,18 +15,60 @@ public:
         }
         return index + 1;
     }
+};
 
-    int removeDuplicates_2(vector<int>& nums) 
+// Use iterator
+// Time: O(n)
+// Space: O(1)
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
     {
         if (nums.empty())
             return 0;
         vector<int>::iterator iter1 = nums.begin();
         vector<int>::iterator iter2 = nums.begin() + 1;
-        for ( ; iter2 != nums.end(); ++iter2)
+        for (; iter2 != nums.end(); ++iter2)
         {
             if (*iter1 != *iter2)
                 *(++iter1) = *iter2;
         }
         return iter1 - nums.begin() + 1;
+    }
+};
+
+// Use STL
+// Time: O(n)
+// Space: O(1)
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        return distance(nums.begin(), unique(nums.begin(), nums.end()));
+    }
+};
+
+// Use STL
+// Time: O(n)
+// Space: O(1)
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        return distance(nums.begin(), removeDuplicates(nums.begin(), nums.end(), nums.begin()));
+    }
+
+    template<typename InIt, typename OutIt>
+    OutIt removeDuplicates(InIt first, InIt last, OutIt output) {
+        while (first != last)
+        {
+            *output++ = *first
+            first = upper_bound(first, last, *first)
+        }
+
+        return output;
     }
 }

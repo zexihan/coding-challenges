ecf683441eba8dcb700f8bed6ab2650d43829fb4
zexihan <zexihan@outlook.com>
2020-02-27 00:21:59 -0800

    add code

---
+++ lintcode/C++/0837-e-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+// 考虑如果substring(i,j)如果是回文串，那么str[i]和str[j]一定相同，并且一定满足以下两个条件之一
+// 1.substring(i+1,j-1)也是回文串
+// 2.j-i<=2，即substring(i,j)长度<=2
+// Time: O(n^2)
+class Solution
+{
+public:
+    int countPalindromicSubstrings(string &str)
+    {
+        int n = str.size();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j <= i; ++j)
+            {
+                dp[i][j] = (str[j] == str[i]) && (i - j <= 2 || dp[i - 1][j + 1]);
+                ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+};
---
+++ lintcode/C++/0838-e-subarray-sum-equals-k.cpp
@@ -0,0 +1,44 @@
+// Time: O(n^2)
+// Space: O(n)
+class Solution_1
+{
+public:
+    int subarraySumEqualsK (vector<int> &nums, int k)
+    {
+        const int n = nums.size();
+        vector<int> sums(n + 1, 0);
+        for (int i = 1; i <= n; ++i)
+            sums[i] = sums[i - 1] + nums[i - 1];
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = i; j < n; ++j)
+                if (sums[j + 1] - sums[i] == k)
+                    ++ans;
+        return ans;
+    }
+};
+
+// Let sum = nums[0] + ... + nums[i]
+// Check how many prefix sums (j's, j < i) equal to sum - k
+// nums[0] + ... + nums[j] = sum - k
+// then there are the same number of j's that nums[j + 1] + ... + nums[i] = k
+// Time: O(n)
+// Space: O(n)
+class Solution_2
+{
+public:
+    int subarraySumEqualsK (vector<int> &nums, int k)
+    {
+        if (nums.empty()) return 0;
+        unordered_map<int, int> counts{{0, 1}};
+        int ans = 0;
+        int sum = 0;
+        for (const int num : nums)
+        {
+            sum += num;
+            ans += counts[sum - k];
+            ++counts[sum];
+        }
+        return ans;
+    }
+};

9f13b5990744e519a9ec6182632935173db6aeca
zexihan <zexihan@outlook.com>
2020-06-03 22:01:01 -0700

    Update solutions

---
+++ leetcode_151_cpp/5.1.4-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,55 @@
+// Recursive
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        vector<vector<int>> result;
+        traverse(root, 1, result);
+        return result;
+    }
+private:
+    void traverse(TreeNode *root, size_t level, vector<vector<int>> &result) {
+        if (!root)
+            return;
+        
+        if (level > result.size())
+            result.push_back(vector<int>());
+        
+        result[level-1].push_back(root->val);
+        traverse(root->left, level+1, result);
+        traverse(root->right, level+1, result);
+    }
+};
+
+// Iterative
+// Time: O(n)
+// Space: O(1)
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        queue<TreeNode *> q;
+
+        if (root == nullptr)
+            return result;
+        else
+            q.push(root);
+        
+        while (!q.empty()) {
+            vector<int> level;
+            size_t size = q.size();
+            for (size_t i = 0; i < size; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if (node->left != nullptr)
+                    q.push(node->left);
+                if (node->right != nullptr)
+                    q.push(node->right);
+            }
+            result.push_back(level);
+        }
+        return result;
+    }
+};

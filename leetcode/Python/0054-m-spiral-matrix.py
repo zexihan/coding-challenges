e0d524f966a80f23ff798b3bb212e03a996e315a
zexihan <zexihan@outlook.com>
2020-06-16 18:03:41 -0700

    Update a solution

--- leetcode/Python/0054-m-spiral-matrix.py
+++ leetcode/Python/0054-m-spiral-matrix.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         def spiral_coords(r1, c1, r2, c2):
             for c in range(c1, c2 + 1):
@@ -20,4 +20,42 @@ class Solution:
                 ans.append(matrix[r][c])
             r1 += 1; r2 -= 1
             c1 += 1; c2 -= 1
+        return ans
+
+class Solution_2:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        if not matrix[0]:
+            return []
+        m = len(matrix)
+        n = len(matrix[0])
+        ans = [0] * (m*n)
+        
+        trace = [[False]*n for i in range(m)]
+        x, y = 0, 0
+        # direction: 0:right 1:down 2:left: 3:up
+        direction = 0
+        i = 1
+        ans[0] = matrix[x][y]
+        trace[0][0] = True
+        while i < (m*n):
+            bkup_x, bkup_y = x, y
+            if direction == 0:
+                y += 1
+            elif direction == 1:
+                x += 1
+            elif direction == 2:
+                y -= 1
+            elif direction == 3:
+                x -= 1
+
+            if x < 0 or y < 0 or x >= m or y >= n or trace[x][y]:
+                direction = (direction + 1) %4
+                x, y = bkup_x, bkup_y
+                continue
+
+            ans[i] = matrix[x][y]
+            trace[x][y] = True
+            i += 1
         return ans
